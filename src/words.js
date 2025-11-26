1c968f7e1c6d4f4744ce159d0202aa0775c02892
patrickrosecode <87778782+patrickrosecode@users.noreply.github.com>
2025-11-26 20:07:00 +0000

    Added more days

--- src/words.js
+++ src/words.js
@@ -1,11 +1,100 @@
 export const words =  [
-    { date: '2025-11-25'
-        , word1: 'over' 
-        , word2: 'time' 
-        , word3: 'piece' 
-        , word4: 'together' 
-        , word5: 'forever' 
-        , word6: 'young' 
+    { date: '2025-11-26',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
     },
 
+    { date: '2025-11-27',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-11-28',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-11-29',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-11-30',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-12-01',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-12-02',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-12-03',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-12-04',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-12-05',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
+
+    { date: '2025-12-06',
+        word1: 'over', 
+        word2: 'time', 
+        word3: 'piece', 
+        word4: 'together', 
+        word5: 'forever', 
+        word6: 'young' 
+    },
 ];
